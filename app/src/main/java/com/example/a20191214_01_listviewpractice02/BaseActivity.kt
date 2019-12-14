d9f84f8109dfbb9ad5aea682a1c55b9adfb9370c
csh0117 <tnsgur!053>
2019-12-14 11:13:00 +0900

    aa

---
+++ app/src/main/java/com/example/a20191214_01_listviewpractice02/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.example.a20191214_01_listviewpractice02
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+
+}
